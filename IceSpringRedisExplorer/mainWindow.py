544444df371a9de3f6b6295fed209a3308404a13
BaiJiFeiLong <baijifeilong@gmail.com>
2022-03-01 18:10:17 +0800

    Update requirements.txt

--- IceSpringRedisExplorer/mainWindow.py
+++ IceSpringRedisExplorer/mainWindow.py
@@ -3,6 +3,7 @@ import json
 import logging
 
 import redis
+from IceSpringRealOptional.typingUtils import gg
 from PySide2 import QtWidgets, QtCore, QtGui
 
 
@@ -13,26 +14,25 @@ class MainWindow(QtWidgets.QMainWindow):
         self.statusBar().showMessage("Ready.")
 
     def _setupView(self):
-        self._mainSplitter = QtWidgets.QSplitter()
         self._treeView = QtWidgets.QTreeView()
         self._treeView.setHeaderHidden(True)
         self._treeView.setAlternatingRowColors(True)
-        self._treeView.setEditTriggers(QtWidgets.QTreeView.NoEditTriggers)
+        self._treeView.setEditTriggers(gg(QtWidgets.QTreeView.NoEditTriggers))
         self._treeView.clicked.connect(self._refreshNode)
         self._treeView.doubleClicked.connect(self._onTreeViewDoubleClicked)
-        self._detailSplitter = QtWidgets.QSplitter(QtCore.Qt.Vertical, self._mainSplitter)
+        self._treeModel = QtGui.QStandardItemModel()
+        self._treeView.setModel(self._treeModel)
+        self._detailSplitter = QtWidgets.QSplitter(gg(QtCore.Qt.Vertical))
+        self._mainSplitter = QtWidgets.QSplitter()
         self._mainSplitter.addWidget(self._treeView)
         self._mainSplitter.addWidget(self._detailSplitter)
         self._mainSplitter.setStretchFactor(0, 1)
         self._mainSplitter.setStretchFactor(1, 1)
         self.setCentralWidget(self._mainSplitter)
 
-        self._treeModel = QtGui.QStandardItemModel(self._treeView)
-        self._treeView.setModel(self._treeModel)
-
-        self._infoEdit = QtWidgets.QTextEdit("Info", self._detailSplitter)
-        self._keyEdit = QtWidgets.QTextEdit("Key", self._detailSplitter)
-        self._valueWidget = QtWidgets.QWidget(self._detailSplitter)
+        self._infoEdit = QtWidgets.QTextEdit("Info")
+        self._keyEdit = QtWidgets.QTextEdit("Key")
+        self._valueWidget = QtWidgets.QWidget()
         self._detailSplitter.addWidget(self._infoEdit)
         self._detailSplitter.addWidget(self._keyEdit)
         self._detailSplitter.addWidget(self._valueWidget)
@@ -40,37 +40,30 @@ class MainWindow(QtWidgets.QMainWindow):
         self._detailSplitter.setStretchFactor(1, 1)
         self._detailSplitter.setStretchFactor(2, 2)
 
-        self._valueLayout = QtWidgets.QVBoxLayout(self._valueWidget)
-        self._valueLayout.setMargin(0)
-        self._valueLayout.setSpacing(0)
-        self._valueWidget.setLayout(self._valueLayout)
-        self._valueEdit = QtWidgets.QTextEdit("Value", self._detailSplitter)
-        self._valueRadioLayout = QtWidgets.QHBoxLayout(self._valueWidget)
-        self._valueLayout.addWidget(self._valueEdit)
-        self._valueLayout.addLayout(self._valueRadioLayout)
-        self._valueAutoRadio = QtWidgets.QRadioButton("Auto", self._valueWidget)
+        self._valueEdit = QtWidgets.QTextEdit("Value")
+        self._valueRadioLayout = QtWidgets.QHBoxLayout()
+        self._valueAutoRadio = QtWidgets.QRadioButton("Auto")
         self._valueAutoRadio.setChecked(True)
-        self._valueRawRadio = QtWidgets.QRadioButton("Raw", self._valueWidget)
-        self._valueJsonRadio = QtWidgets.QRadioButton("JSON", self._valueWidget)
-        self._valueRadioGroup = QtWidgets.QButtonGroup(self._valueLayout)
+        self._valueRawRadio = QtWidgets.QRadioButton("Raw")
+        self._valueJsonRadio = QtWidgets.QRadioButton("JSON")
+        self._valueRadioGroup = QtWidgets.QButtonGroup()
         self._valueRadioGroup.buttonClicked.connect(self._refreshValue)
         for radio in self._valueAutoRadio, self._valueRawRadio, self._valueJsonRadio:
             self._valueRadioLayout.addWidget(radio)
             self._valueRadioGroup.addButton(radio)
         self._valueRadioLayout.addStretch()
+        self._valueLayout = QtWidgets.QVBoxLayout()
+        self._valueLayout.setMargin(0)
+        self._valueLayout.setSpacing(0)
+        self._valueLayout.addWidget(self._valueEdit)
+        self._valueLayout.addLayout(self._valueRadioLayout)
+        self._valueWidget.setLayout(self._valueLayout)
 
         sources = ["Alpha", "Beta", "Gamma"]
         for source in sources:
             sourceNode = QtGui.QStandardItem(source)
-            sourceNode.setData(dict(type="SOURCE", host="localhost", port=6379, password=None), QtCore.Qt.UserRole)
+            sourceNode.setData(dict(type="SOURCE", host="localhost", port=6379, password=None), gg(QtCore.Qt.UserRole))
             self._treeModel.invisibleRootItem().appendRow(sourceNode)
-
-        self._treeModel = self._treeModel
-        self._treeView = self._treeView
-        self._infoEdit = self._infoEdit
-        self._valueRadioGroup = self._valueRadioGroup
-        self._keyEdit = self._keyEdit
-        self._valueEdit = self._valueEdit
 
     def _onTreeViewDoubleClicked(self, modelIndex: QtCore.QModelIndex):
         clickedNode = self._treeModel.itemFromIndex(modelIndex)
@@ -80,11 +73,11 @@ class MainWindow(QtWidgets.QMainWindow):
             for key in rds.info("KEYSPACE"):
                 db = int(key[2:])
                 childNode = QtGui.QStandardItem(str(db))
-                childNode.setData(dict(type="SCHEMA", db=db), QtCore.Qt.UserRole)
+                childNode.setData(dict(type="SCHEMA", db=db), gg(QtCore.Qt.UserRole))
                 clickedNode.appendRow(childNode)
         if jd["type"] == "SCHEMA" and not clickedNode.hasChildren():
             self._generateChildrenForNode(clickedNode, self._generateTreeDictFromKeys(rds.keys()))
-            self._treeView.sortByColumn(0, QtCore.Qt.AscendingOrder)
+            self._treeView.sortByColumn(0, gg(QtCore.Qt.AscendingOrder))
 
     @staticmethod
     def _generateTreeDictFromKeys(keys):
@@ -101,12 +94,12 @@ class MainWindow(QtWidgets.QMainWindow):
             parentPath = (node.data(QtCore.Qt.UserRole) or dict()).get("path", b"")
             childPath = f"{parentPath.decode()}:{k}".lstrip(":").encode()
             childNode = QtGui.QStandardItem(k)
-            childNode.setData(dict(type="KEY", path=childPath), QtCore.Qt.UserRole)
+            childNode.setData(dict(type="KEY", path=childPath), gg(QtCore.Qt.UserRole))
             node.appendRow(childNode)
             self._generateChildrenForNode(childNode, v)
 
     def _refreshNode(self, index: QtCore.QModelIndex):
-        jd = index.data(QtCore.Qt.UserRole)
+        jd = index.data(gg(QtCore.Qt.UserRole))
         if jd["type"] != "KEY":
             return
         key = jd["path"]
@@ -136,9 +129,9 @@ class MainWindow(QtWidgets.QMainWindow):
     def _refreshValue(self, ):
         key = self._infoEdit.property("key")
         value = self._infoEdit.property("value")
-        type = self._valueRadioGroup.checkedButton().text()
-        type = type if type != "Auto" else self._detectTextFormat(value.decode())
-        text = json.dumps(self._loadJson(value), indent=4, ensure_ascii=False) if type == "JSON" else value.decode()
+        _type = self._valueRadioGroup.checkedButton().text()
+        _type = _type if _type != "Auto" else self._detectTextFormat(value.decode())
+        text = json.dumps(self._loadJson(value), indent=4, ensure_ascii=False) if _type == "JSON" else value.decode()
         self._keyEdit.setText(key.decode())
         self._valueEdit.setText(text)
 
